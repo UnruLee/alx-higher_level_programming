8b672f8a10c522d3b1c75bae1cd1e4a07ba73a87
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-08-09 05:34:36 -0700

    print numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{:d}".format(i), end=", ")
-    if i >= 10:
-        print("{:d}".format(i), end=", ")
+    if i < 99:
+        print("{:02}".format(i), end=", ")
+    else:
+        print("99 \n")
