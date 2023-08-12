566d55573b84812e1a4afd5184daca4f41f67043
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-08-12 15:58:33 -0700

    list arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+    l = len(sys.argv)
+    n = l - 1
+    v = (sys.argv)
+
+    if n == 0:
+        print("{} arguments".format(n), end='.\n')
+    elif n == 1:
+        print("{} argument".format(n), end=':\n')
+        print("{}:".format(n), v[n])
+    else:
+        print("{} arguments".format(n), end=':\n')
+        for q in range(n):
+                print("{}: {}".format(q + 1, v[q + 1]))
