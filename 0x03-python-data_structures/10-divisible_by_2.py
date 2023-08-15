7dee02774f7b0cd86744af75be7bf20a0b409322
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-08-15 16:26:12 -0700

    divisible by 2 list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    list_result = my_list[:]
+    list_result = my_list.copy()
 
-    for x in range(0, len(list_result)):
-        if x % 2 == 0:
+    for x in range(len(list_result)):
+        if list_result[x] % 2 == 0:
             list_result[x] = True
         else:
             list_result[x] = False
