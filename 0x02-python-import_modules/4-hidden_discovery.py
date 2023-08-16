3a1c288b50a3f6c3d5b1210d6f9e7328485b244e
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-08-16 13:45:02 -0700

    hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        import hidden_4
+
+        names = dir(hidden_4)
+        for x in names:
+            if x[0] != "_" and x[1] != "_":
+                print(x)
